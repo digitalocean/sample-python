f5109b4f625545354fd94433c563b1c4c2d4658e
Steven Normore <snormore@digitalocean.com>
2020-01-23 13:35:49 -0500

    Fix server.py

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.wfile.write(msg.encode())
 
 
-port = os.getenv('PORT', 80)
+port = int(os.getenv('PORT', 80))
 print('Listening on port %s' % (port))
 httpd = socketserver.TCPServer(('', port), Handler)
 httpd.serve_forever()
