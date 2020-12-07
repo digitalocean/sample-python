43b291cdd217b661c570ebc90ec5d8021896b6f2
natronpc <nathan@autismcandles.com>
2020-12-07 12:51:22 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,21 @@
-#!/usr/bin/env python3
+import asyncio
+import websockets
 
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print(f"< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f"> {greeting}")
+
+start_server = websockets.serve(hello, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+
+'''
 import os
 import http.server
 import socketserver
@@ -11,7 +27,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
+        msg = 'AutismChat.org2 you requested %s' % (self.path)
         self.wfile.write(msg.encode())
 
 
@@ -19,3 +35,4 @@ port = int(os.getenv('PORT', 80))
 print('Listening on port %s' % (port))
 httpd = socketserver.TCPServer(('', port), Handler)
 httpd.serve_forever()
+'''
