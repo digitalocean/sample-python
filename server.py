f41cf34cf1c907d86f252bd01044e60a1865a04a
Pandu07x <68238527+Pandu07x@users.noreply.github.com>
2022-05-27 22:50:58 +0530

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
+        msg = 'Hello! you requestedsss %s' % (self.path)
         self.wfile.write(msg.encode())
 
 
