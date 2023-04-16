00847878f68cb99946901ebbcb8348dc1bd952dd
Abul-Aiman-Shaa <aimanshaay3k@gmail.com>
2023-04-16 12:25:00 +0530

    Created a context manager and added "main" stamp

--- server.py
+++ server.py
@@ -6,14 +6,15 @@ from http import HTTPStatus
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
+    def __init__(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
         msg = 'Hello! you requested %s' % (self.path)
         self.wfile.write(msg.encode())
 
 
-port = int(os.getenv('PORT', 80))
-print('Listening on port %s' % (port))
-httpd = socketserver.TCPServer(('', port), Handler)
-httpd.serve_forever()
+if __name__ == "__main__":
+    port = int(os.getenv('PORT', 80))
+    print('Listening on port %s' % (port))
+    httpd = socketserver.TCPServer(('', port), Handler)
+    httpd.serve_forever()
