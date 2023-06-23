cc85a9ddd9324a3a8d7f320be180a3769706db32
Jignesh <jc.mca1985@gmail.com>
2023-06-23 17:09:13 +0530

    First changes

---
+++ server - Copy.py
@@ -0,0 +1,19 @@
+import os
+import http.server
+import socketserver
+
+from http import HTTPStatus
+
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        msg = 'Hello! you requested %s' % (self.path)
+        self.wfile.write(msg.encode())
+
+
+port = int(os.getenv('PORT', 80))
+print('Listening on port %s' % (port))
+httpd = socketserver.TCPServer(('', port), Handler)
+httpd.serve_forever()
