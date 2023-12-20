be086734f39437a411c11d883b1fa16736161f76
chattylab <136754071+chattylab@users.noreply.github.com>
2023-12-21 01:04:40 +0200

    tiday test

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello! you requested NOW %s' % (self.path)
+        msg = 'Hello! you requested today %s' % (self.path)
         self.wfile.write(msg.encode())
 
 
