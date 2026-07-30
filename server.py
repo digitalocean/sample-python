423326a8f85e1668803d9120346c76fe0ccef3b6
Kingfish Solutions <mcurtis@kingfishsolutions.com>
2026-07-30 14:19:27 +1000

    Update greeting message in server response

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello Buddy!!!!! MBC HERE ON 30TH JUL AGAIN YO YO YO%s' % (self.path)
+        msg = 'Hello Buddy!!!!! MBC HERE ON 30TH JUL AGAIN YO YO YO SCOOBY DOO%s' % (self.path)
         self.wfile.write(msg.encode())
 
 
