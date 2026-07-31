50dfec1cb06c05687f5042c7e63992b72952fa5d
Kingfish Solutions <mcurtis@kingfishsolutions.com>
2026-07-31 14:09:34 +1000

    Change response to HTML with content type header
    Updated the HTTP response to send an HTML page instead of a plain text message. Added content type header and structured HTML response.

--- server.py
+++ server.py
@@ -8,9 +8,28 @@ from http import HTTPStatus
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
+        self.send_header("Content-Type", "text/html; charset=utf-8")
         self.end_headers()
-        msg = 'Hello Buddy!!!!! MBC HERE ON 30TH JUL AGAIN NOW 31ST JULY%s' % (self.path)
-        self.wfile.write(msg.encode())
+
+        html_content = f"""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MBC Status</title>
+    <style>
+        body {{ font-family: Arial, sans-serif; text-align: center; margin-top: 50px; background-color: #f4f4f9; color: #333; }}
+        h1 {{ color: #0066cc; }}
+        p {{ font-size: 1.2em; }}
+    </style>
+</head>
+<body>
+    <h1>Hello Buddy!!!!!</h1>
+    <p>MBC HERE ON 30TH JUL AGAIN NOW 31ST JULY</p>
+    <p><strong>Path requested:</strong> {self.path}</p>
+</body>
+</html>
+"""
+self.wfile.write(html_content.encode("utf-8"))
 
 
 port = int(os.getenv('PORT', 80))
